69d033eeb5b8c128e8459658628677808b64b6fa
Thomas Okken <thomasokken@gmail.com>
2021-02-08 00:37:11 -0500

    Added note about simpleserver/favicon.ico

--- icon/convert.sh
+++ icon/convert.sh
@@ -4,6 +4,9 @@
 # That is a multi-resolution format, don't know how to deal with that.
 # I used https://icoconvert.com/ on a transparent original,
 # and that seems to work fine.
+# I used Gimp to create iphone/simpleserver/favicon.ico, selecting
+# 8 bpp output. Maybe that can also be automated using Netpbm.
+# Notice that this file has a rather different format from Free42.ico.
 
 OPAQUE="$1"
 TRANSPARENT="$2"
